f16500676981c1f6ae5890ad6d2482c0ae8e5c39
Shaojun Shen <137772740@qq.com>
2022-03-25 21:13:46 +0800

    初始化前端

---
+++ front-end/src/pages/IndexPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-page class="flex_1">
+    <q-table
+      title="Treats"
+      :rows="rows"
+      :columns="columns"
+      row-key="name"
+      :pagination="initialPagination"
+    />
+  </q-page>
+</template>
+
+<script>
+import { defineComponent, reactive } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage',
+
+})
+</script>
